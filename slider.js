58d6a101a364b5623e885bc7b67d1b12d59010e1
Roman_Bryzgalin <rbryzgalin@gmail.com>
2023-11-12 07:35:04 +0700

    Конечный вариант

--- slider.js
+++ slider.js
@@ -9,71 +9,76 @@ area = ["81 m2", "105 m2", "93 m2"];
 time = ["3.5 months", "4 months", "3 months"];
 
 //ФУНКЦИЯ ПЕРЕКЛЮЧЕНИЯ setActive 
-function setActive(parameter) {   //Задаем функцию с параметр parameter
-    console.log(parameter);
+function setActive(index) {
+    console.log(index);
 
-    for (let k = 1; k <= 3; k++) {  // начало, условие, шаг цикла
+    for (let k = 1; k <= 3; k++) {
         console.log(k);
 
-        if (k === parameter) { // если k строго равно parameter то делай
-            document.querySelector(`.n${k}`).classList.add("gold_text"); //для навигации добавляй gold_text
-            document.querySelector(`.n${k}_navi_icon`).setAttribute('fill-opacity', 1); //для точки установи значение fill-opacity', 1
-        } else { //или
-            document.querySelector(`.n${k}`).classList.remove("gold_text");//для навигации убирай gold_text
-            document.querySelector(`.n${k}_navi_icon`).setAttribute('fill-opacity', 0.3);//для точки установи значение fill-opacity', 0.3
+        if (k === index) {
+            document.querySelector(`.n${k}`).classList.add("gold_text");
+            document.querySelector(`.n${k}_navi_icon`).setAttribute('fill-opacity', 1);
+        } else {
+            document.querySelector(`.n${k}`).classList.remove("gold_text");
+            document.querySelector(`.n${k}_navi_icon`).setAttribute('fill-opacity', 0.3);
         }
     }
 
-    //находим и присваиваем
-    document.querySelector(".slider_photo_block__picture").src = images[parameter - 1]; //меняем на новую картинку с индексом минус 1
-    //отнимаем от параметра 1 так как массив начинается с 0
-    document.querySelector(".slider_photo_block__picture").setAttribute("data-parameter", `${parameter}`); //меняем значение атрибута data-parameter на значение индекс полученное ранее
-    document.querySelector(".city").innerHTML = city[parameter - 1]; //в сити вставляем новое значение из массива минус 1 по счету
-    document.querySelector(".dept").innerHTML = dept[parameter - 1];// аналогично для других текстов
-    document.querySelector(".area").innerHTML = area[parameter - 1];// аналогично для других текстов
-    document.querySelector(".time").innerHTML = time[parameter - 1];// аналогично для других текстов
+    document.querySelector(".slider_photo_block__picture").src = images[index - 1];
+    document.querySelector(".slider_photo_block__picture").setAttribute("data-index", `${index}`);
+    document.querySelector(".city").innerHTML = city[index - 1];
+    document.querySelector(".dept").innerHTML = dept[index - 1];
+    document.querySelector(".area").innerHTML = area[index - 1];
+    document.querySelector(".time").innerHTML = time[index - 1];
 }
 
 //ОБРАБОТКА КЛИКОВ НА СТРЕЛКИ
-left_arrow.addEventListener("click", function () { //слышит клик на left_arrow
-    const elem = document.querySelector(".slider_photo_block__picture"); //находит картинку и присваивает в elem
-    const parameter = elem.getAttribute("data-parameter");
-    if (parameter === "1") { //если parameter равно 1
-        elem.setAttribute("data-parameter", `${images.length}`); //выставляй картинку с data-parameter равной номеру из массива
-        setActive(images.length); //запускай ФУНКЦИЮ ПЕРЕКЛЮЧЕНИЯ с аргументом images.length
+left_arrow.addEventListener("click", function () {
+
+    const elem = document.querySelector(".slider_photo_block__picture");
+    const index = elem.getAttribute("data-index");
+
+    if (index === "1") {
+        elem.setAttribute("data-index", `${images.length}`);
+        setActive(images.length);
     } else {
-        elem.setAttribute("data-parameter", `${parameter - 1}`);
-        setActive(+parameter - 1); //parameter делай числом и отнимай 1 
+        elem.setAttribute("data-index", `${index - 1}`);
+        setActive(+index - 1);
     };
 });
 
-right_arrow.addEventListener("click", function () { //слышит клик на right_arrow
+right_arrow.addEventListener("click", function () {
+
     const elem = document.querySelector(".slider_photo_block__picture")
-    const parameter = elem.getAttribute("data-parameter");
-    if (parameter === `${images.length}`) {
-        elem.setAttribute("data-parameter", "1");
+    const index = elem.getAttribute("data-index");
+
+    if (index === `${images.length}`) {
+        elem.setAttribute("data-index", "1");
         setActive(1);
     } else {
-        elem.setAttribute("data-parameter", `${parameter + 1}`);
-        setActive(+parameter + 1);
+        elem.setAttribute("data-index", `${index + 1}`);
+        setActive(+index + 1);
     };
 });
 
 //ОБРАБОТКА КЛИКОВ НА НАВИГАЦИОННЫЕ ССЫЛКИ
-let item = document.getElementsByClassName("slider_photo_block__link");  //находит все элементы с классом slider_photo_block__link
+let item = document.getElementsByClassName("slider_photo_block__link");
+
 for (let i = 0; i < item.length; i++) {
-    item[i].addEventListener("click", function () { //слышит клик на item[i]
-        setActive(i + 1); // запускаем нашу функцию с аргументом i + 1 
-        //прибавляем к порядковому номера массива 1, т.к. массив начинается с 0
+    item[i].addEventListener("click", function () {
+
+        setActive(i + 1);
+
     })
 }
 
 //ОБРАБОТКА КЛИКОВ НА НАВИГАЦИОННЫЕ ТОЧКИ
-let itemdot = document.getElementsByClassName("navi__dot"); //находит все элементы с классом navi__dot
+let itemdot = document.getElementsByClassName("navi__dot");
+
 for (let i = 0; i < item.length; i++) {
     itemdot[i].addEventListener("click", function () {
 
-        setActive(i + 1); //прибавляем к порядковому номера массива 1, т.к. массив начинается с 0
+        setActive(i + 1);
 
     })
 }
